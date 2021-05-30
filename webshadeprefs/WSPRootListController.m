c32d2a28209e806fe307233203a4e46347072db3
WilsontheWolf <33164598+WilsontheWolf@users.noreply.github.com>
2021-05-29 18:03:41 -0600

    WIP v0.0.3

---
+++ webshadeprefs/WSPRootListController.m
@@ -0,0 +1,13 @@
+#include "WSPRootListController.h"
+
+@implementation WSPRootListController
+
+- (NSArray *)specifiers {
+	if (!_specifiers) {
+		_specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+	}
+
+	return _specifiers;
+}
+
+@end
---
+++ webshadeprefs/WSPSitesListController.h
@@ -0,0 +1,13 @@
+#import <Preferences/PSListController.h>
+#import <Preferences/PSTableCell.h>
+#import <Preferences/PSSpecifier.h>
+#import <Preferences/PSSliderTableCell.h>
+#import <CepheiPrefs/HBRootListController.h>
+#import <CepheiPrefs/HBAppearanceSettings.h>
+#import <Cephei/HBRespringController.h>
+#import <Cephei/HBPreferences.h>
+
+
+@interface WSPSitesListController : HBRootListController
+@property (nonatomic, retain) NSMutableDictionary *wordReplacements;
+@end
